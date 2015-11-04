2a8ccbef2939857665c0ab25bd3f7b2780838607
Sebastian Nette <sebastian-nette@gmx.de>
2015-11-04 01:13:45 +0100

    Update content.js

--- content.js
+++ content.js
@@ -1,4 +1,250 @@
 // content.js
+
+var countries = [  
+   "All Countries",
+   "Albania",
+   "Algeria",
+   "American Samoa",
+   "Andorra",
+   "Angola",
+   "Anguilla",
+   "Antarctica",
+   "Antigua and Barbuda",
+   "Argentina",
+   "Armenia",
+   "Aruba",
+   "Australia",
+   "Austria",
+   "Azerbaijan",
+   "Bahamas",
+   "Bahrain",
+   "Bangladesh",
+   "Barbados",
+   "Belarus",
+   "Belgium",
+   "Belize",
+   "Benin",
+   "Bermuda",
+   "Bhutan",
+   "Bolivia",
+   "Bosnia and Herzegovina",
+   "Botswana",
+   "Bouvet Island",
+   "Brazil",
+   "British Indian Ocean Territory",
+   "British Virgin Islands",
+   "Brunei Darussalam",
+   "Bulgaria",
+   "Burkina Faso",
+   "Burundi",
+   "Cambodia",
+   "Cameroon",
+   "Canada",
+   "Cape Verde",
+   "Cayman Islands",
+   "Central African Republic",
+   "Chad",
+   "Chile",
+   "China",
+   "Christmas Island",
+   "Cocos (Keeling) Islands",
+   "Colombia",
+   "Comoros",
+   "Congo",
+   "Congo, the Democratic Republic of the",
+   "Cook Islands",
+   "Costa Rica",
+   "Cote d",
+   "Croatia",
+   "Cyprus",
+   "Czech Republic",
+   "Denmark",
+   "Djibouti",
+   "Dominica",
+   "Dominican Republic",
+   "Ecuador",
+   "Egypt",
+   "El Salvador",
+   "Equatorial Guinea",
+   "Eritrea",
+   "Estonia",
+   "Ethiopia",
+   "Falkland Islands",
+   "Faroe Islands",
+   "Fiji",
+   "Finland",
+   "France",
+   "French Guiana",
+   "French Polynesia",
+   "French Southern and Antarctic Lands",
+   "Gabon",
+   "Gambia",
+   "Georgia",
+   "Germany",
+   "Ghana",
+   "Gibraltar",
+   "Greece",
+   "Greenland",
+   "Grenada",
+   "Guadeloupe",
+   "Guam",
+   "Guatemala",
+   "Guinea",
+   "Guinea-Bissau",
+   "Guyana",
+   "Haiti",
+   "Heard Island and McDonald Islands",
+   "Holy See",
+   "Honduras",
+   "Hong Kong",
+   "Hungary",
+   "Iceland",
+   "India",
+   "Indonesia",
+   "Ireland",
+   "Isle of Man",
+   "Israel",
+   "Italy",
+   "Jamaica",
+   "Japan",
+   "Jordan",
+   "Kazakhstan",
+   "Kenya",
+   "Kiribati",
+   "Kuwait",
+   "Kyrgyzstan",
+   "Laos",
+   "Latvia",
+   "Lebanon",
+   "Lesotho",
+   "Liechtenstein",
+   "Lithuania",
+   "Luxembourg",
+   "Macao",
+   "Macedonia",
+   "Madagascar",
+   "Malawi",
+   "Malaysia",
+   "Maldives",
+   "Mali",
+   "Malta",
+   "Marshall Islands",
+   "Martinique",
+   "Mauritania",
+   "Mauritius",
+   "Mayotte",
+   "Mexico",
+   "Micronesia, Federated States of",
+   "Moldova",
+   "Monaco",
+   "Mongolia",
+   "Montenegro",
+   "Montserrat",
+   "Morocco",
+   "Mozambique",
+   "Myanmar",
+   "Namibia",
+   "Nepal",
+   "Netherlands",
+   "Netherlands Antilles",
+   "New Caledonia",
+   "New Zealand",
+   "Nicaragua",
+   "Niger",
+   "Nigeria",
+   "Niue",
+   "Norfolk Island",
+   "Northern Mariana Islands",
+   "Norway",
+   "Oman",
+   "Pakistan",
+   "Palau",
+   "Palestinian Territories",
+   "Panama",
+   "Papua New Guinea",
+   "Paraguay",
+   "Peru",
+   "Philippines",
+   "Pitcairn",
+   "Poland",
+   "Portugal",
+   "Puerto Rico",
+   "Qatar",
+   "Reunion",
+   "Romania",
+   "Russia",
+   "Rwanda",
+   "Saint Helena",
+   "Saint Kitts and Nevis",
+   "Saint Lucia",
+   "Saint Pierre and Miquelon",
+   "Saint Vincent and the Grenadines",
+   "Samoa",
+   "San Marino",
+   "Sao Tome and Principe",
+   "Saudi Arabia",
+   "Senegal",
+   "Serbia",
+   "Seychelles",
+   "Sierra Leone",
+   "Singapore",
+   "Slovakia",
+   "Slovenia",
+   "Solomon Islands",
+   "Somalia",
+   "South Africa",
+   "South Korea",
+   "Spain",
+   "Sri Lanka",
+   "Suriname",
+   "Svalbard and Jan Mayen",
+   "Swaziland",
+   "Sweden",
+   "Switzerland",
+   "Taiwan",
+   "Tajikistan",
+   "Tanzania",
+   "Thailand",
+   "Timor-Leste",
+   "Togo",
+   "Tokelau",
+   "Tonga",
+   "Trinidad and Tobago",
+   "Tunisia",
+   "Turkey",
+   "Turkmenistan",
+   "Turks and Caicos Islands",
+   "Tuvalu",
+   "Uganda",
+   "Ukraine",
+   "United Arab Emirates",
+   "United Kingdom",
+   "United States",
+   "United States Minor Outlying Islands",
+   "United States Virgin Islands",
+   "Uruguay",
+   "Uzbekistan",
+   "Vanuatu",
+   "Venezuela",
+   "Vietnam",
+   "Wallis and Futuna",
+   "Western Sahara",
+   "Yemen",
+   "Zambia",
+   "Zimbabwe"
+];
+
+// client info style
+var style = {
+    float: 'right',
+    width: 200,
+    background: '#fff',
+    border: '1px #ccc solid',
+    padding: 4,
+    marginLeft: 10,
+    marginBottom: 10
+};
+
 $(function() {
 
     var buttonLocation = 'aside.oSide';
@@ -6,31 +252,68 @@ $(function() {
     // job feed
     if (window.location.href.indexOf("find-work-home") !== -1) {
         buttonLocation = '.oLayout aside';
+        style.marginBottom = 0;
     }
+
+    // jquery button location
+    var $buttonLocation = $(buttonLocation).first();
     
     // remove placeholder text
     $(buttonLocation + ' h1').first().remove();
 
+    // countries
+    var select = $('<select style="width:100%; margin-bottom:10px;">');
+        select.on("keyup change", hideJobsByCountry);
+        select.prependTo($buttonLocation);
+
+    // add options
+    countries.forEach(function(country) {
+        $('<option>').val(country).text(country).appendTo(select);
+    });
+
     // power search button
-    $('<span id="grab_info" class="oBtn oBtnPrimary" style="display:block; margin-bottom:10px;">Search Power-Up</span>')
+    $('<span class="oBtn oBtnPrimary" style="display:block; margin-bottom:10px;">Search Power-Up</span>')
         .click(StartScrappingProcess)
-        .prependTo($(buttonLocation).first());
+        .prependTo($buttonLocation);
 
-    // client info style
-    var style = {
-        float: 'right',
-        width: 200,
-        background: '#fff',
-        border: '1px #ccc solid',
-        padding: 4,
-        marginLeft: 10,
-        marginBottom: 10
-    };
+    // hide jobs by country
+    function hideJobsByCountry() {
 
+        var value = $(this).val();
+        var showAll = value === countries[0];
+
+        $('.hidden-job-placeholder').remove();
+
+        $('.oJobTile').each(function() {
+            
+            var element = $(this);
+            var record = element.find('.oSpendIndicator i').attr('title');
+
+            // hide
+            if (showAll || record.indexOf(value) !== -1) {
+                element.find('.oSupportInfo, .oDescription, .oFormInfo, .oSegmentationInfo').show();
+            } else {
+                element.find('.oSupportInfo, .oDescription, .oFormInfo, .oSegmentationInfo').hide();
+
+                var placeholder = $('<div class="hidden-job-placeholder">');
+                    placeholder.text('This job has been hidden. Click to view.');
+                    placeholder.css('cursor', 'pointer');
+                    placeholder.click(function() {
+                        element.find('.oSupportInfo, .oDescription, .oFormInfo, .oSegmentationInfo').show();
+                        placeholder.remove();
+                    });
+                    placeholder.appendTo(element);
+            }
+        });
+    }
+
+    // data scraping
     function StartScrappingProcess() {
 
+        // remove previous content
         $('.power-up').remove();
  
+        // get data
         $('.oJobTile').each(function() {
 
             var element = $(this);
@@ -58,7 +341,7 @@ $(function() {
             }
 
             // application data
-            $.getJSON("https://www.upwork.com/jobs/" + $(this).data("id") + "/applicants", function(data) {
+            $.getJSON("https://www.upwork.com/jobs/" + $(this).attr("data-id") + "/applicants", function(data) {
 
                 var appCount = data.applicants.length;
                 var interviewCount = data.invitedToInterview;
